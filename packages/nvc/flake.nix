452b335b6c4383c3f76b994e0e7e103ded0f9723
Michael Koller <koller_michael@outlok.de>
2024-04-23 20:56:35 +0200

    update packages/nvc/flake.*

--- packages/nvc/flake.nix
+++ packages/nvc/flake.nix
@@ -4,7 +4,7 @@
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.nvc =
       let pkgs = import nixpkgs {
-            system = "x86_64-linux";
+        system = "x86_64-linux";
           };
       in pkgs.stdenv.mkDerivation {
         pname = "nvc";
@@ -12,49 +12,48 @@
         version = "1.11.3";
 
         src = pkgs.fetchFromGitHub {
-	  owner = "nickg";
-	  repo = "nvc";
-	  rev = "r1.11.3";
- 	  hash = "sha256-Z4YxXPf8uKlASSK9v6fbtHtkUibc9EeA4i+3kD/vBmY=";
+          owner = "nickg";
+          repo = "nvc";
+          rev = "r1.11.3";
+          hash = "sha256-Z4YxXPf8uKlASSK9v6fbtHtkUibc9EeA4i+3kD/vBmY=";
         };
 
-	preConfigure = ''
-	  mkdir build
-	  cd build
-	'';
+        preConfigure = ''
+          mkdir build
+          cd build
+        '';
 
-	configureScript = "../configure";
+        configureScript = "../configure";
 
-	configureFlags = [
-	  "--enable-vhpi"
-	  "--disable-lto"
-	];
-
-	# an alternative to the above three attributes is the following:
-	# (note that --prefix=$out then needs to be specified)
-        #configurePhase = ''
-	#  mkdir build
- 	#  cd build
-	#  ../configure --enable-vhpi --disable-lto --prefix="$out"
-	#'';
-
-        nativeBuildInputs = with pkgs; [
-	  autoreconfHook
-	  check
-	  elfutils
-	  flex
-	  libffi
-	  pkg-config
-	  llvm
-	  which
-	  zlib
-	  zstd
+        configureFlags = [
+          "--enable-vhpi"
+          "--disable-lto"
         ];
 
-	doCheck = true;
-      };
+        # an alternative to the above three attributes is the following:
+        # (note that --prefix=$out then needs to be specified)
+        # configurePhase = ''
+        #   mkdir build
+        #   cd build
+        #   ../configure --enable-vhpi --disable-lto --prefix="$out"
+        # '';
 
-  defaultPackage.x86_64-linux = self.packages.x86_64-linux.nvc;
+        nativeBuildInputs = with pkgs; [
+          autoreconfHook
+          check
+          elfutils
+          flex
+          libffi
+          pkg-config
+          llvm
+          which
+          zlib
+          zstd
+        ];
+
+        doCheck = true;
+      };
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.nvc;
   };
 }
 
