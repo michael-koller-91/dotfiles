174c22234941b7772d02f7c4be57c2c37d4eb5e4
Michael Koller <koller_michael@outlook.de>
2025-07-19 21:13:12 +0200

    update apt_install.sh

--- apt_install.sh
+++ apt_install.sh
@@ -6,6 +6,9 @@ apt install bat -y
 apt install eza
 apt install fd-find
 apt install gcc -y
+apt install git
+apt install konsole
 apt install make
 apt install ripgrep
 apt install unzip
+apt install zsh -y
