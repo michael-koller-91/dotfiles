998133dd73b3333709a8f48222a7952651ab1e22
Michael Koller <koller_michael@outlook.de>
2025-07-19 16:36:25 +0200

    add neovim/install_release.sh

---
+++ neovim/install_release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir downloads
+wget -P downloads https://github.com/neovim/neovim/releases/download/v0.11.3/nvim-linux-x86_64.tar.gz
+tar -xvzf downloads/nvim-linux-x86_64.tar.gz -C downloads
+ln -s $PWD/downloads/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+rm -rf downloads/nvim-linux-x86_64.tar.gz
