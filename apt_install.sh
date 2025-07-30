fcb671be457213915e5c3d4713a4eba42ea1c37a
Michael Koller <koller_michael@outlook.de>
2025-07-30 16:53:21 +0200

    update apt_install.sh

--- apt_install.sh
+++ apt_install.sh
@@ -12,7 +12,9 @@ apt install python3-pip -y
 apt install python3-venv -y
 apt install konsole
 apt install make
+apt install mtpaint -y
 apt install nfs-common -y
+apt install onedrive -y
 apt install ripgrep
 apt install texlive-fonts-extra -y
 apt install texlive-latex-extra -y
